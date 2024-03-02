6fe3e8d821532bee89291f2281fddab9d768028f
break1145 <breakgoogle@163.com>
2024-03-02 18:41:51 +0800

    Implemented the LinkedListDeque class

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,154 @@
+package deque;
+
+public class LinkedListDeque<Type> {
+
+    private static class dequeNode<T>{
+        public T val;
+        dequeNode<T> pre, nxt;
+
+        public dequeNode(T val) {
+            this.val = val;
+            this.pre = null;
+            this.nxt = null;
+        }
+        /**
+         * @param val node's value
+         * @param pre pre node
+         * @param nxt next node
+         * */
+        public dequeNode(T val, dequeNode<T> pre,dequeNode<T> nxt) {
+            this.val = val;
+            this.pre = pre;
+            this.nxt = nxt;
+        }
+
+    }
+
+    // last <-> sentinel <-> First <->sentinel1 <-> last
+    private dequeNode<Type> sentinel;
+    private dequeNode<Type> sentinel1;
+    private int size;
+
+    public LinkedListDeque() {
+        sentinel = new dequeNode<>(null);
+        sentinel1 = new dequeNode<>(null,sentinel, sentinel);
+        sentinel.pre = sentinel1;
+        sentinel.nxt = sentinel1;
+        size = 0;
+    }
+
+    /**
+     * last <-> sentinel <-> First <->sentinel1 <-> last
+     * @param val value to addLast
+     * @return value to add
+     */
+    public Type addLast(Type val) {
+        size += 1;
+        dequeNode<Type> newNode = new dequeNode<>(val);
+        dequeNode<Type> p = sentinel.pre;
+        sentinel.pre = newNode;
+        newNode.pre = p;
+        newNode.nxt = sentinel;
+        if(p != null) {
+            p.nxt = newNode;
+        }
+        if(newNode.pre == null) {
+            newNode.pre = sentinel1;
+            sentinel1.nxt = newNode;
+        }
+
+        return val;
+    }
+
+    /**
+     * last <-> sentinel <-> First <->sentinel1 <-> last
+     * @param val value to addFirst
+     * @return value to add
+     */
+    public Type addFirst(Type val) {
+        size += 1;
+        dequeNode<Type> newNode = new dequeNode<>(val);
+        dequeNode<Type> p = sentinel.nxt;
+        sentinel.nxt = newNode;
+        newNode.nxt = p;
+        newNode.pre = sentinel;
+        if(p != null) {
+            p.pre = newNode;
+        }
+        if(newNode.nxt == null) {
+            newNode.nxt = sentinel1;
+            sentinel1.pre = newNode;
+        }
+        return val;
+    }
+
+    public int size() {return size;}
+
+    public Type getLast() {return sentinel.pre.val;}
+
+    public Type getFirst() {return sentinel.nxt.val;}
+
+    /**
+     * remove and delete the last node of the list
+     * */
+    public Type removeLast() {
+        if(size == 0){return null;}
+        size -= 1;
+        if(sentinel.pre == sentinel1) {
+            dequeNode<Type> p = sentinel1.pre.pre;
+            Type val = sentinel1.pre.val;
+            sentinel1.pre = null;
+            p.nxt = sentinel1;
+            sentinel1.pre = p;
+            return val;
+        }
+        dequeNode<Type> p = sentinel.pre.pre;
+        Type val = sentinel.pre.val;
+        sentinel.pre = null;
+        p.nxt = sentinel;
+        sentinel.pre = p;
+        return val;
+    }
+
+    /**
+     * remove and delete the first node of the list
+     * */
+    public Type removeFirst() {
+        if(size == 0){return null;}
+        size -=1;
+
+        if(sentinel.nxt == sentinel1) {
+            dequeNode<Type> p = sentinel1.nxt.nxt;
+            Type val = sentinel1.nxt.val;
+            p.pre = sentinel1;
+            sentinel1.nxt = p;
+            return val;
+        }
+        dequeNode<Type> p = sentinel.nxt.nxt;
+        Type val = sentinel.nxt.val;
+        p.pre = sentinel;
+        sentinel.nxt = p;
+        return val;
+    }
+
+    public String printDeque() {
+        StringBuilder str = new StringBuilder();
+        dequeNode<Type> p = sentinel;
+        while(p .nxt != sentinel) {
+            p = p.nxt;
+            if (p.val == null) {continue;}
+            str.append(p.val);
+            str.append(' ');
+            System.out.printf(p.val.toString() + ' ');
+        }
+        System.out.println();
+        return str.toString();
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+
+}
