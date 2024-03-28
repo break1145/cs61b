7c9b7a940b70221f2690d0ff44aa7deda5c42034
break1145 <breakgoogle@163.com>
2024-03-28 17:19:57 +0800

    Enhance the .gitlet structure Implement add function preliminarily

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,6 +1,11 @@
 package gitlet;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
 import static gitlet.Utils.*;
 
 // TODO: any imports you need here
@@ -24,11 +29,18 @@ public class Repository {
     public static final File CWD = new File(System.getProperty("user.dir"));
     /** The .gitlet directory. */
     public static final File GITLET_DIR = join(CWD, ".gitlet");
+    public static final File Staging_Area = join(GITLET_DIR, "staging");
+    public static final File Commit_DIR = join(GITLET_DIR, "commits");
+    public static final File Files_DIR = join(GITLET_DIR, "blobs");
+
 
     /* TODO: fill in the rest of this class. */
 
     public static void setupFileFolder() {
         GITLET_DIR.mkdir();
+        Staging_Area.mkdir();
+        Commit_DIR.mkdir();
+        Files_DIR.mkdir();
     }
 
     /**
@@ -40,9 +52,13 @@ public class Repository {
             System.out.println("A Gitlet version-control system already exists in the current directory.");
             System.exit(0);
         }
-
         setupFileFolder();
-        //TODO fill in the init command
+
+        HashSet<String> stagingArea= new HashSet<>();
+        writeObject(Staging_Area, stagingArea);
+
+        // TODO: fill in the init command after 'commit' implement
+        // make first commit with no content
     }
 
     /** Adds a copy of the file as it currently exists to the staging area.<p>
@@ -53,6 +69,24 @@ public class Repository {
             System.out.println("File does not exist.");
             System.exit(0);
         }
+
+        HashSet<File> stagingArea = readObject(Staging_Area, HashSet.class);
+        //TODO: check if the file is same as the latest commit's
+        // get file from commit
+        //File latestFile =
+        try {
+            // deal with if file is same
+            compareFiles(file, file);
+            return;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // overwrite if exist
+        if(stagingArea.contains(file)) {
+            stagingArea.remove(file);
+        }
+        stagingArea.add(file);
+        // TODO: deal with case if 'rm'
 
     }
 }
