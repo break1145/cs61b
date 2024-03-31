64f2954a1d0744ab88b95b162118788fc090e49a
break1145 <breakgoogle@163.com>
2024-03-31 11:29:01 +0800

    add testCommit

---
+++ proj2/gitlet/UnitTests.java
@@ -0,0 +1,70 @@
+package gitlet;
+
+
+import org.junit.Test;
+
+import java.io.File;
+import java.util.HashSet;
+
+import static gitlet.Utils.*;
+import static gitlet.Repository.*;
+public class UnitTests {
+    @org.junit.Test
+    public void testAddCommit() {
+        CommitTree CT = new CommitTree();
+        CT.add_Commit(new Commit("0   commit"));
+        CT.add_Commit(new Commit("1st commit"));
+        CT.add_Commit(new Commit("2nd commit"));
+        CT.add_Commit(new Commit("3rd commit"));
+        CT.printTree();
+    }
+    @org.junit.Test
+    public void testFileGet() {
+        File CWD = new File(System.getProperty("user.dir"));
+        File f = join(CWD, "gitlet-design.md");
+
+    }
+
+    @Test
+    public void testInitialize() {
+        Repository.initialize();
+    }
+
+    @org.junit.Test
+    public void testCommit() {
+
+        CommitTree commitTree = new CommitTree();
+        HashSet<Blob> stagingArea = new HashSet<>();
+        HashSet<Blob> removedStagingArea = new HashSet<>();
+        writeObject(CommitTree_DIR_File, commitTree);
+        writeObject(Removed_Staging_Area_File, removedStagingArea);
+        writeObject(Staging_Area_File, stagingArea);
+
+        commit("initial commit");
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
