57d16cd26817bcaa2291c2005885a0c5ef1d82ce
break1145 <breakgoogle@163.com>
2024-04-21 12:52:49 +0800

    add unitTests for LCA bugs have not been fixed

--- proj2/gitlet/UnitTests.java
+++ proj2/gitlet/UnitTests.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.io.File;
 import java.util.HashSet;
+import java.util.List;
 
 import static gitlet.Repository.*;
 import static gitlet.Utils.*;
@@ -106,7 +107,73 @@ public class UnitTests {
         System.out.println(b1.getFile().getName());
 //        System.out.println(b2.getShaCode());
     }
+    @Test
+    public void testBranch() {
+        startCheck();
+//        add(new File("gitlet/testmore"));
+//        commit("new file testmore");
+        branch currentBranch = readObject(join(Branch_DIR, "master"), branch.class);
+        List<String> commitList = currentBranch.commitList;
+        for(String commitID : commitList) {
+            Commit commit1 = readObject(join(Commit_DIR, commitID), Commit.class);
+            System.out.printf(commit1.getHashCode() + ' ');
+            System.out.println(commit1.getMessage());
+        }
 
+    }
+
+    @Test
+    public void testLCA(){
+        initialize();
+        CommitTree commitTree = readObject(CommitTree_DIR_File, CommitTree.class);
+        commitTree.setCurrentBranch("master");
+        // case0
+        commitTree.add_Commit(new Commit("0   commit"));
+        commitTree.add_Commit(new Commit("1st commit"));
+        Commit commit2 = new Commit("2nd commit");
+        commitTree.add_Commit(commit2);
+        //branch newBranch = new branch("newB", commit2.getHashCode());
+
+        branch("newB");
+        commitTree.setCurrentBranch("newB");
+        commitTree.add_Commit(new Commit("3rd commit"));
+        branch newB = readObject(join(Branch_DIR, "newB"), branch.class);
+//        message(commitTree.getHeadCommit().getHashCode());
+//        message(newB.getStartCommitID());
+        commitTree.add_Commit(new Commit("4th commit"));
+        commitTree.add_Commit(new Commit("5th commit"));
+        commitTree.setCurrentBranch("master");
+        commitTree.add_Commit(new Commit("6th commit"));
+        commitTree.add_Commit(new Commit("7th commit"));
+
+
+        Commit LCA = getSplitPoint("master", "newB");
+        System.out.println("");
+        message(LCA.getMessage());
+        message(LCA.getHashCode());
+        commitTree.printTree();
+
+    }
+    @Test
+    public void testLCA2(){
+        Commit LCA = getSplitPoint("master", "newB");
+        message(LCA.getMessage());
+        message(LCA.getHashCode());
+        message("");
+        branch master = readObject(join(Branch_DIR, "master"), branch.class);
+        branch newB = readObject(join(Branch_DIR, "newB"), branch.class);
+        for(String item: master.commitList) {
+            message(item);
+        }
+        message("");
+        for(String item: newB.commitList) {
+            message(item);
+        }
+        message("");
+
+        message(master.getStartCommitID());
+        message(newB.getStartCommitID());
+    }
 
 }
 
