232b8c146fb1833114741c36e59b9297fd98629c
break1145 <breakgoogle@163.com>
2024-05-27 09:02:49 +0800

    bug fixs

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -16,7 +16,7 @@ public class Main {
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
 
         if(GITLET_DIR.isDirectory()) {
             Repository.startCheck();
@@ -65,7 +65,11 @@ public class Main {
                 break;
             case "merge":
                 branch given = readObject(join(Branch_DIR, args[1]), branch.class);
-                Repository.merge(given);
+                try {
+                    Repository.merge(given);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
                 break;
             case "status":
                 Repository.status();
